245ccc80afcfc984fa9c40c24ec86fa654d64bbb
cyxigo <cyxigo@proton.me>
2026-04-10 23:08:16 +0300

    REDESIGN!!! There's a lot to clean up though... Eh...

--- scripts/components/MenuBar.js
+++ scripts/components/MenuBar.js
@@ -4,112 +4,27 @@ export default class MenuBar extends HTMLElement {
   }
 
   connectedCallback() {
-    this.isMenuVisible = false;
-
-    this.createTitle();
-    this.createButtons();
-    this.setupStyles();
-    this.createButtonsContainer();
-    this.createToggleButton();
-    this.updateMenuVisibility();
-  }
-
-  setupStyles() {
-    Array.from(this.children).forEach((child) => {
-      child.style.margin = "0";
-      child.style.padding = "14px 16px";
-      child.style.color = "white";
-    });
-  }
-
-  createTitle() {
-    const title = document.createElement("h2");
-    title.innerText = "cyxigo";
-
-    this.appendChild(title);
-  }
-
-  createButtons() {
-    const buttons = [];
     const changeLocation = (location) => {
       document.location = "/" + location;
     };
 
-    const aboutMe = document.createElement("button");
-    aboutMe.innerText = "About Me";
-    aboutMe.onclick = () => changeLocation("");
+    const about = document.createElement("button");
+    about.innerText = "About";
+    about.style.borderBottomColor = "#969696";
+    about.onclick = () => changeLocation("");
 
-    const myMusic = document.createElement("button");
-    myMusic.innerText = "My Music";
+    const music = document.createElement("button");
+    music.innerText = "Music";
+    music.style.borderBottomColor = "#5ae6ff";
+    music.onclick = () => changeLocation("music");
 
-    myMusic.onclick = () => changeLocation("music");
+    const projects = document.createElement("button");
+    projects.innerText = "Projects";
+    projects.style.borderBottomColor = "#ff5757";
+    projects.onclick = () => changeLocation("projects");
 
-    const myProjects = document.createElement("button");
-    myProjects.innerText = "My Projects";
-    myProjects.onclick = () => changeLocation("projects");
-
-    buttons.push(aboutMe, myMusic, myProjects);
-    this.buttons = buttons;
-  }
-
-  createButtonsContainer() {
-    const buttonsContainer = document.createElement("div");
-
-    this.buttons.forEach((button) => {
-      button.style.backgroundColor = "#606060";
-      button.style.transition = "0.2s ease-in-out";
-      button.style.padding = "6px 16px";
-      button.style.textAlign = "left";
-      button.style.fontSize = "12px";
-
-      button.onmouseenter = () => {
-        button.style.backgroundColor = "#474747ff";
-      };
-
-      button.onmouseleave = () => {
-        button.style.backgroundColor = "#606060";
-      };
-
-      buttonsContainer.appendChild(button);
-    });
-
-    buttonsContainer.style.flexDirection = "column";
-    buttonsContainer.style.backgroundColor = "#606060";
-
-    this.buttonsContainer = buttonsContainer;
-    this.appendChild(this.buttonsContainer);
-  }
-
-  createToggleButton() {
-    const toggleButton = document.createElement("button");
-    toggleButton.innerHTML = "☰";
-
-    toggleButton.style.backgroundColor = "#9c9c9cff";
-    toggleButton.style.transition = "0.2s ease-in-out";
-    toggleButton.style.fontSize = "22px";
-    toggleButton.style.fontFamily = "Arial";
-    toggleButton.style.borderBottomLeftRadius = "6px";
-    toggleButton.style.borderBottomRightRadius = "6px";
-
-    toggleButton.onclick = () => {
-      this.isMenuVisible = !this.isMenuVisible;
-      this.updateMenuVisibility();
-    };
-
-    toggleButton.onmouseenter = () => {
-      toggleButton.style.backgroundColor = "#838383ff";
-    };
-
-    toggleButton.onmouseleave = () => {
-      toggleButton.style.backgroundColor = "#9c9c9cff";
-    };
-
-    this.toggleButton = toggleButton;
-    this.appendChild(this.toggleButton);
-  }
-
-  updateMenuVisibility() {
-    this.buttonsContainer.style.display = this.isMenuVisible ? "flex" : "none";
-    this.toggleButton.innerHTML = this.isMenuVisible ? "✕" : "☰";
+    this.appendChild(about);
+    this.appendChild(music);
+    this.appendChild(projects);
   }
 }
